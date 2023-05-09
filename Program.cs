b90c6693a86534a5c248f3086ecaa5991eaf935d
jhon <jhon@DESKTOP-B67EJTJ>
2023-05-09 11:20:13 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace VariabelOpgave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fornavn = "Malthe";
+            //Indexed output
+            Console.WriteLine("Your name is: {0}", fornavn);
+            //Concated output
+            Console.WriteLine("Your name is: " + fornavn);
+            //Interpoled 
+            Console.WriteLine($"Your name is: {fornavn}");
+
+            //Waits for key-press before ending program
+            Console.ReadKey();
+        }
+    }
+    
+}
